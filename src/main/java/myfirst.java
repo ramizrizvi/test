60f8be0840220275ed6a987fd4c77be80e451d33
ramizrizvi <syedramizhussainrizvi@gmail.com>
2021-06-28 12:09:43 +0500

    Showwwdown

--- src/main/java/myfirst.java
+++ src/main/java/myfirst.java
@@ -2,7 +2,7 @@
 public class myfirst {
     public static void main(String[] args) {
         System.out.println("Good");
-        System.out.println("hello!!!");
+        System.out.println("hello!!!!");
 
         Student obj = new Student();
         obj.meth1();
