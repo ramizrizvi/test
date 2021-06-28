31f4481c93cc1940f4a1ac875576d523ee5e7046
ramizrizvi <syedramizhussainrizvi@gmail.com>
2021-06-28 11:38:24 +0500

    JJJJ

---
+++ src/main/java/Student.java
@@ -0,0 +1,6 @@
+
+public class Student {
+    public void meth1(){
+        System.out.println("this is meth1");
+    }
+}
--- src/main/java/myfirst.java
+++ src/main/java/myfirst.java
@@ -8,5 +8,8 @@ public class myfirst {
     public static void main(String[] args) {
         System.out.println("Good");
         System.out.println("hello!!!");
+
+        Student obj = new Student();
+        obj.meth1();
     }
 }
