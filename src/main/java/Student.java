ebfaca428e2709623f4314d09e03a2958d9badce
ramizrizvi <syedramizhussainrizvi@gmail.com>
2021-07-07 13:46:33 +0500

    wednesday

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,7 +1,6 @@
 
 public class Student {
     public void meth1(){
-        System.out.println("this is meth1");
-        System.out.println("meth1 speaking");
+        System.out.println("Hello Welcome!!!!");
     }
 }
